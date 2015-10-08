7663e8e7f789584d62cfd35d6b0f1054fb6813f9
Gopal Divya <divya7891@gmail.com>
2015-10-08 15:15:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ZUI5_Test1
+ZUI5_Test1 by harshada
---
+++ util/messages.js
@@ -0,0 +1,57 @@
+jQuery.sap.declare("try.util.messages");
+jQuery.sap.require("sap.ca.ui.message.message");
+
+try.util.messages = {};
+
+/**
+ * Show an error dialog with information from the oData response object.
+ *
+ * @param {object}
+ *            oParameter The object containing error information
+ * @return {void}
+ * @public
+ */
+try.util.messages.showErrorMessage = function(oParameter) {
+	var oErrorDetails =
+		try.util.messages._parseError(oParameter);
+	var oMsgBox = sap.ca.ui.message.showMessageBox({
+		type: sap.ca.ui.message.Type.ERROR,
+		message: oErrorDetails.sMessage,
+		details: oErrorDetails.sDetails
+	});
+	if (!sap.ui.Device.support.touch) {
+		oMsgBox.addStyleClass("sapUiSizeCompact");
+	}
+};
+
+try.util.messages.getErrorContent = function(oParameter) {
+	return try.util.messages._parseError(oParameter).sMessage;
+};
+
+try.util.messages._parseError = function(oParameter) {
+	var sMessage = "",
+		sDetails = "",
+		oEvent = null,
+		oResponse = null,
+		oError = {};
+
+	if (oParameter.mParameters) {
+		oEvent = oParameter;
+		sMessage = oEvent.getParameter("message");
+		sDetails = oEvent.getParameter("responseText");
+	} else {
+		oResponse = oParameter;
+		sMessage = oResponse.message;
+		sDetails = oResponse.response.body;
+	}
+
+	if (jQuery.sap.startsWith(sDetails, "{\"error\":")) {
+		var oErrModel = new sap.ui.model.json.JSONModel();
+		oErrModel.setJSON(sDetails);
+		sMessage = oErrModel.getProperty("/error/message/value");
+	}
+
+	oError.sDetails = sDetails;
+	oError.sMessage = sMessage;
+	return oError;
+};
